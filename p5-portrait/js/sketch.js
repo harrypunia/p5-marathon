bce5f7635774fdef4fa3d566a1ed5ef0e54c9d4a
Harry <harryrock67@gmail.com>
2019-01-17 17:03:24 -0500

    p5 portrait boilerplate established

---
+++ p5-portrait/js/sketch.js
@@ -0,0 +1,58 @@
+let init = false,
+    reset = false,
+    col = {
+        0: {
+            r: 30,
+            g: 10,
+            b: 22
+        },
+        1: {
+            r: 250,
+            g: 73,
+            b: 60
+        },
+        2: {
+            r: 82,
+            g: 10,
+            b: 20
+        },
+        3: {
+            r: 155,
+            g: 120,
+            b: 120
+        },
+        4: {
+            r: 222,
+            g: 23,
+            b: 120
+        },
+    },
+    img;
+
+
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight);
+    if (0 == 0) { //Condition here
+        let btn = document.getElementById('play');
+        btn.classList.add('in');
+    }
+    img = loadImage('assets/main.jpg');
+}
+
+function draw() {
+    reset ? (background(col[0].r, col[0].g, col[0].b), reset = false) : background(col[0].r, col[0].g, col[0].b, 90);
+
+    if (init) {}
+}
+
+function windowResized() {
+    reset = true;
+    resizeCanvas(window.innerWidth, window.innerHeight);
+}
+
+
+const initSketch = () => {
+    init = true;
+    let btn = document.getElementById('play');
+    btn.style.display = 'none';
+}
