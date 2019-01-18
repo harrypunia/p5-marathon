2eeaa679ca59778829903409a0107baca1bdece6
Harry <harryrock67@gmail.com>
2019-01-17 21:47:50 -0500

    Fixed image resolution and added mutliple color objects

--- p5-portrait/js/sketch.js
+++ p5-portrait/js/sketch.js
@@ -1,9 +1,41 @@
 let init = false,
     reset = false,
     col = {
-        r: 150,
-        g: 20,
-        b: 50
+        1: {
+            r: 150,
+            g: 20,
+            b: 50
+        },
+        2: {
+            r: 10,
+            g: 10,
+            b: 0
+        },
+        3: {
+            r: 10,
+            g: 10,
+            b: 0
+        },
+        4: {
+            r: 10,
+            g: 10,
+            b: 0
+        },
+        5: {
+            r: 10,
+            g: 10,
+            b: 0
+        },
+        6: {
+            r: 10,
+            g: 10,
+            b: 0
+        },
+        7: {
+            r: 10,
+            g: 10,
+            b: 0
+        }
     },
     _img1_ = {
         lB1: [],
@@ -50,7 +82,7 @@ let init = false,
     img3,
     img4,
     speed = 2;
-    population = 50;
+population = 50;
 
 function preload() {
     img1 = loadImage('assets/main' + Math.floor(random(11)) + '.jpg');
@@ -62,10 +94,10 @@ function preload() {
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
     smooth();
-    img1.resize(width/4, 0);
-    img2.resize(width/4, 0);
-    img3.resize(width/4, 0);
-    img4.resize(width/4, 0);
+    img1.resize(width / 4, 0);
+    img2.resize(width / 4, 0);
+    img3.resize(width / 4, 0);
+    img4.resize(width / 4, 0);
     if (0 == 0) {
         let btn = document.getElementById('play');
         btn.classList.add('in');
@@ -75,9 +107,9 @@ function setup() {
     img2.loadPixels();
     img3.loadPixels();
     img4.loadPixels();
-    
+
     background(0)
-    
+
     setParticles(_img1_, img1);
     setParticles(_img2_, img2);
     setParticles(_img3_, img3);
