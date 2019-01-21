a2a22b0cfb8d2465230aec72403510d2277d3e7e
Harry <harryrock67@gmail.com>
2019-01-21 17:23:33 -0500

    christmas tree pattern mapping to volume,
    //functional prototype

--- p5-tenticles/js/Tenticle.js
+++ p5-tenticles/js/Tenticle.js
@@ -4,16 +4,16 @@ class Tenticle {
         this.angle = 0;
     }
     show(vol) {
-        console.log(vol);
         stroke(255);
         applyMatrix();
         translate(width / 2, height / 2);
+        let mapVol = vol < .5 ? vol : .5;
         for (let i = 0; i < this.n; i++) {
-            let posX = (i + 1) * Math.sin(this.angle + (i * 5)),
+            let posX = (i + 1) * Math.sin(this.angle + i),
                 posY = i * 2;
             point(posX, posY);
         }
-        this.angle += 0.05;
+        this.angle += mapVol;
         resetMatrix();
     }
 }
