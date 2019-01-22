043653369942688885a442a078fd9f4b3d25b746
Harry <harryrock67@gmail.com>
2019-01-21 19:32:39 -0500

    corrected math for line and added noise

--- p5-crazyLines/js/CrazyLine.js
+++ p5-crazyLines/js/CrazyLine.js
@@ -1,16 +1,19 @@
 class CrazyLine {
-    constructor(x, y, l) {
+    constructor(x, y, l, i) {
         this.transX = x;
         this.transY = y;
         this.length = l;
-        this.yOff = 0;
+        this.xoff = i * 100;
+        this.rotateVal;
     }
     show(vol) {
         stroke(255);
+        this.rotateVal = map(noise(this.xoff), 0, 1, 0, 360);
         applyMatrix();
         translate(this.transX, this.transY);
-        rotate(vol);
-        line(0, 0, this.length, this.length);
+        rotate(Math.PI * this.rotateVal);
+        line(0, 0, 0, this.length);
         resetMatrix();
+        this.xoff += 0.01;
     }
 }
