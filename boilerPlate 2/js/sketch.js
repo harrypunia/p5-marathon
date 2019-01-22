4482562053861e68e7b85541dc0488726a14d965
Harry <harryrock67@gmail.com>
2019-01-22 12:21:50 -0500

    New project created

---
+++ boilerPlate 2/js/sketch.js
@@ -0,0 +1,66 @@
+let init = false,
+    reset = false,
+    col = {
+        0: {
+            r: 30,
+            g: 10,
+            b: 22
+        },
+        1: {
+            r: 250,
+            g: 73,
+            b: 60
+        },
+        2: {
+            r: 82,
+            g: 10,
+            b: 20
+        },
+        3: {
+            r: 155,
+            g: 120,
+            b: 120
+        },
+        4: {
+            r: 222,
+            g: 23,
+            b: 120
+        },
+    },
+    song,
+    amp,
+    vol;
+
+function preload() {
+    song = loadSound('assets/song.mp3');
+}
+
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight);
+    if (song.isLoaded()) { //Condition here
+        let btn = document.getElementById('play');
+        btn.classList.add('in');
+    }
+    amp = new p5.Amplitude();
+}
+
+function draw() {
+    reset ? (background(col[0].r, col[0].g, col[0].b), reset = false) : background(col[0].r, col[0].g, col[0].b, 90);
+
+    if (init) {
+        amp.getLevel();
+    }
+}
+
+function windowResized() {
+    reset = true;
+    resizeCanvas(window.innerWidth, window.innerHeight);
+}
+
+
+const initSketch = () => {
+    init = true;
+    song.play();
+    let btn = document.getElementById('play');
+    btn.style.display = 'none';
+}
