1cae31ab5f136e20837f8d125c9ec251bbf6433a
Harry <harryrock67@gmail.com>
2019-01-24 23:00:22 -0500

    project completed

--- p5-minato/js/TanCircle.js
+++ p5-minato/js/TanCircle.js
@@ -11,27 +11,37 @@ class TanCircle {
             length: 200,
             rot: 0
         };
+        this.angle;
     }
-    show(vol, r, g, b) {
+    show(vol, r, g, b, i) {
         this.r = this.storeR + vol;
-        this.updateLine();
+        this.updateLine(i);
         stroke(r, g, b);
         noFill();
         push();
         translate(this.x, this.y);
-        rotate(this.line.angle);
-        strokeWeight(1);
+        rotate(this.angle);
         line(this.line.x - this.line.length, this.line.y, this.line.x + this.line.length, this.line.y);
-        strokeWeight(4);
         point(this.line.x + this.line.length, this.y);
         point(this.line.x - this.line.length, this.y);
+        point(this.x - this.line.length, this.line.y);
+        point(this.x + this.line.length, this.line.y);
+        point(this.line.x + this.line.length, this.y);
+        point(this.line.x - this.line.length, this.y);
+
+        point(this.x, this.line.y - this.line.length);
+        point(this.x, this.line.y) + this.line.length;
+        point(this.line.x, this.y + this.line.length);
+        point(this.line.x, this.y - this.line.length);
+        point(this.x, this.line.y - this.line.length);
+        point(this.x, this.line.y + this.line.length);
         pop();
     }
-    updateLine() {
-        this.line.angle += 0.005;
+    updateLine(i) {
+        this.line.angle += 0.01;
         this.x = this.r * Math.sin(this.line.angle) + (width / 2);
         this.y = this.r * Math.cos(this.line.angle) + (height / 2);
         let pos = this.x + this.y;
-        this.line.rot = map(pos, -(this.r + width / 2) * 2, (this.r + width / 2) * 2, 0, 3.14);
+        this.angle = map(mouseX, 0, width, 0, 3.14) + this.line.angle;
     }
 }
--- p5-minato/js/sketch.js
+++ p5-minato/js/sketch.js
@@ -34,9 +34,11 @@ let init = false,
     rOff = 0,
     gOff = 1000,
     bOff = 1000000,
+    sOff = 1000,
     colR,
     colG,
-    colB;
+    colB,
+    pSize;
 
 function preload() {
     song = loadSound('assets/song.mp3');
@@ -58,16 +60,17 @@ function draw() {
     reset ? (background(col[0].r, col[0].g, col[0].b), reset = false) : background(col[0].r, col[0].g, col[0].b, 90);
 
     if (init) {
-        rOff++
-        gOff++
-        bOff++
-        colR = map(noise(rOff), 0, 1, 0, 100);
-        colG = map(noise(gOff), 0, 1, 0, 100);
+        rOff += 0.01;
+        gOff += 0.01;
+        bOff += 0.01;
+        sOff += 0.01;
+        colR = map(noise(rOff), 0, 1, 0, 200);
+        colG = map(noise(gOff), 0, 1, 0, 255);
         colB = map(noise(bOff), 0, 1, 0, 100);
-        fill(0);
+        pSize = map(noise(sOff), 0, 1, 0, 100);
         vol = amp.getLevel();
         for (let i = 0; i < tanCircles.length; i++) {
-            tanCircles[i].show(vol * 100, colR, colG, colB);
+            tanCircles[i].show((vol * 50) + pSize, colR, colG, colB, i);
         }
     }
 }
