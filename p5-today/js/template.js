2eebee5862efa2465e02c4525ed6fd37768ccb98
Harry <harryrock67@gmail.com>
2019-06-11 14:35:07 -0400

    init new project

---
+++ p5-today/js/template.js
@@ -0,0 +1,34 @@
+let sketch, song, mp3, init = false, cRotate, eNoise;
+
+function preload() {
+    song = loadSound('assets/song.mp3');
+}
+
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight);
+    document.getElementById('play').classList.add('in');
+    mp3 = new easyMP3(song);
+    cRotate = new easyRotate(width/2, height/2, 40, .01);
+    eNoise = new easyNoise(0, 20, 200);
+}
+
+function draw() {
+    background(30, 10, 22);
+    eNoise.update(0.1);
+    if(init) {
+        noFillStroke(255);
+        star(width/2, height/2, 20, 7);
+        triangle(width/2, height/2, eNoise.val);
+    }
+}
+
+function windowResized() {
+    resizeCanvas(window.innerWidth, window.innerHeight);
+}
+
+const initSketch = () => {
+    song.play();
+    init = true;
+    let btn = document.getElementById('play');
+    btn.style.display = 'none';
+}
